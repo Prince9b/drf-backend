09d822011cf5acf2b57e891173fb78e1c8fbde20
Natoyoum Prince <natoyoumpriince@gmail.com>
2026-01-24 02:38:01 +0100

    Initial commit - Django REST backend

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from .views import NoteListCreateView, NoteDeleteView
+
+urlpatterns = [
+    path('notes/', NoteListCreateView.as_view(), name='note-list'),
+    path('notes/delete/<int:pk>/', NoteDeleteView.as_view(), name='delete-note')
+]
